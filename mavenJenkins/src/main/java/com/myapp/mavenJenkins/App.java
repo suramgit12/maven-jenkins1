d3ecc06b6a5ba7dac52adf8567d790697e2aba7e
suramgit <sura2india@gmail.com>
2022-08-20 16:08:55 +0530

    First Version Project Added

---
+++ mavenJenkins/src/main/java/com/myapp/mavenJenkins/App.java
@@ -0,0 +1,13 @@
+package com.myapp.mavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello It is a Maven Project deployed in Jenkins" );
+    }
+}
